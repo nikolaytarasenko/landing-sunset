172166d3df747ec01a8e1a885f7d72bebcd83473
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-10-18 22:10:05 +0300

    animation effects was added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,9 +1,41 @@
-document.addEventListener('scroll', function (e) {
+function init() {
+    linksClickHandler();
+    animateProficiencySection();
+    startAnimate(700);
+}
+
+function linksClickHandler() {
+    const allLinks = document.querySelectorAll('a');
+    const aboutSection = document.querySelector('.about');
+
+    for (let i = 0; i < allLinks.length; i++) {
+        allLinks[i].addEventListener('click', function(e) {
+            e.preventDefault();
+
+            if (this.classList.contains('scroll')) {
+                aboutSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    }
+}
+
+function animateProficiencySection() {
+    document.addEventListener('scroll', function(e) {
+        startAnimate((document.documentElement.clientHeight / 2) - 200);
+    });
+}
+
+function startAnimate(height) {
     const proficiencySection = document.querySelector('.proficiency');
     const proficiencySectionRect =  proficiencySection.getBoundingClientRect();
     const proficiencyCircles = document.querySelectorAll('.proficiency__circle');
 
-    if (proficiencySectionRect.top < document.documentElement.clientHeight / 2) {
+    if (proficiencySectionRect.top < height) {
         Array.from(proficiencyCircles).forEach(circle => circle.style.animation = 'progress 1.5s ease-out forwards');
     }
-});
+}
+
+window.addEventListener('load', init);
--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -27,7 +27,7 @@ module.exports = function () {
                 }),
                 imagemin.svgo(),
                 imagemin.optipng({optimizationLevel: 3}),
-                pngquant({quality: '65-70', speed: 5})
+                pngquant({quality: [1], speed: 5})
             ], {
                 verbose: true
             })))
